2ecd849dece62a07095331f8015c8469d4da1fa1
Sebastian Hörberg <sebi@SebastiansAir3.fritz.box>
2020-06-09 18:54:18 +0200

    firstRender again

--- server/Game.js
+++ server/Game.js
@@ -1,14 +1,12 @@
 class Game{
-    constructor(assets, canvas, extent) {
+    constructor(canvas, extent) {
         this.extent = extent;
-        this.assests = assets;
         this.canvas = canvas;
         this.cellSize = this.canvas.width / this.extent;
         window.setInterval(this.draw.bind(this), 500);
-
     }
     draw(){
-        this.context.fillStyle = 'red'
+        this.context.fillStyle = 'red';
         this.context.fillRect(5 * this.cellSize,4 * this.cellSize,this.cellSize,this.cellSize);
     }
     
