d41b23c3474458e407318415ff8a55e3d9a6d160
ru85dim <Yoanna.Kostadinova@campus.lmu.de>
2020-06-09 18:33:08 +0200

    'monster'

--- client/monster.js
+++ client/monster.js
@@ -1,8 +1,8 @@
-export class Monster{
-    constructor(x, y, color, life) {
-        this.x = x;
-        this.y = y;
-        this.color = color;
-        this.life = life; 
-    }
+export class Monster {
+  constructor(x, y, color, life) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.life = life;
+  }
 }
