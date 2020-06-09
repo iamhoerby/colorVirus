6268feb928c75489c41514e158538efd164dc06d
Janka <hartmannjanka@gmail.com>
2020-06-09 15:56:04 +0200

    Spieler

---
+++ client/spieler.js
@@ -0,0 +1,12 @@
+export class Spieler{
+    constructor(color, lives, position){
+        this.color = color;
+        this.lives = lives;
+        this.position = position;
+        //openDoor()
+        //mixColors()
+        //changeColor()
+        //shoot()
+        //move()
+    }
+}
