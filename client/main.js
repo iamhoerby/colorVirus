f52758e4324bb4ca3549ed4502c7c44a79efebb7
Sebastian Hörberg <sebi@SebastiansAir3.fritz.box>
2020-06-14 19:29:39 +0200

    test room import

--- client/main.js
+++ client/main.js
@@ -5,8 +5,10 @@
  * ECMA-Script 6 specification. It is also sometimes referred
  * to as ES6 imports.
  */
-import { Game } from "../server/Game.js"
+import { Room } from "/Room.js";
+// import { Game } from "../server/Game.js"
 
+new Room(document.getElementById("myCanvaas"), this.extent, 1); 
 new Game(document.getElementById("myCanvas"),8);
 
 const socket = io();
