9e63eed87fc6e5467258f79433d9e503588e5fb1
Sebastian Hörberg <sebi@SebastiansAir3.fritz.box>
2020-06-16 12:43:59 +0200

    added game.js on client

--- client/main.js
+++ client/main.js
@@ -5,20 +5,13 @@
  * ECMA-Script 6 specification. It is also sometimes referred
  * to as ES6 imports.
  */
-import { Room } from "/Room.js";
-import { Spieler } from "/Spieler.js"
-import { Monster } from "/Monster.js"
-// import { Game } from "../server/Game.js"
+import { Game } from "./Game.js"
 
 let canvas = document.getElementById("myCanvas")
 let context = canvas.getContext('2d')
 const socket = io();
 
-new Room(canvas, 64, 1);
-new Spieler(5, 6, 'blue', 3, 'ArrowRight', 64, context, socket);
-// new Monster(10, 5, 'purple', 2, canvas, 64, true);
-
-// new Game(document.getElementById("myCanvas"),8);
+new Game(document.getElementById("myCanvas"),8);
 
 socket.on("connect", () =>
   console.log(`connected to socket.io as ${socket.id}`)
