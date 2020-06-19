a767af3e85e1a255120cbffdccc619bec5e682ec
ru64xat <andrei.zubarev@campus.lmu.de>
2020-06-19 18:22:14 +0200

    update room, 2,versuch

--- client/Room.js
+++ client/Room.js
@@ -1,4 +1,4 @@
-class Room {
+export class Room {
 
     constructor (canvas, extent, number) {
         this.canvas = canvas;
@@ -6,20 +6,35 @@ class Room {
         this.cellSize = this.canvas.width / this.extent;
         this.number = number;
         this.context = this.canvas.getContext('2d');
-        this.colours = ['red','orange','yellow','green','blue','violet'];
-        this.doorColour = this.colours[Math.floor(Math.random() * this.colours.length)];
-        this.door = new Door (this.doorColour, 'Closed', {x: Math.floor(Math.random() * (this.extent - this.extent/4)), y:0}, this.extent, this.cellSize, this.context);
-        this.draw();
+        this.colours = ['red','orange','yellow','green','blue','violet']; //colours for door
+        this.doorColour = this.colours[Math.floor(Math.random() * this.colours.length)]; //picking random colour for door
+        this.door = new Door (this.doorColour, 
+                                'Closed', 
+                                {x: Math.floor(Math.random() * (this.extent - this.extent/4)), y:0}, //random position for door
+                                this.extent, 
+                                this.cellSize, 
+                                this.context);
+        //coordinates x (random) and y (fixed) for obstacles, depending on extent size. Helps to use extent of any value: 16, 32, 64...                        
+        this.randCoord = [  {x : Math.floor(Math.random() * (this.extent - this.extent/8)), y : this.extent/16 + (this.extent/8 * 0)},
+                            {x : Math.floor(Math.random() * (this.extent - this.extent/8)), y : this.extent/16 + (this.extent/8 * 1)},
+                            {x : Math.floor(Math.random() * (this.extent - this.extent/8)), y : this.extent/16 + (this.extent/8 * 2)},
+                            {x : Math.floor(Math.random() * (this.extent - this.extent/8)), y : this.extent/16 + (this.extent/8 * 3)},
+                            {x : Math.floor(Math.random() * (this.extent - this.extent/8)), y : this.extent/16 + (this.extent/8 * 4)},
+                            {x : Math.floor(Math.random() * (this.extent - this.extent/8)), y : this.extent/16 + (this.extent/8 * 5)},
+                            {x : Math.floor(Math.random() * (this.extent - this.extent/8)), y : this.extent/16 + (this.extent/8 * 6)},
+                            {x : Math.floor(Math.random() * (this.extent - this.extent/8)), y : this.extent/16 + (this.extent/8 * 7)}
+                        ]
+        setInterval(this.draw(), 500);
     }
 
-    drawLine(x1,y1,x2,y2) {
+    drawLine(x1,y1,x2,y2) {     // function for simple line drawing
        this.context.beginPath();
        this.context.moveTo(x1,y1);
        this.context.lineTo(x2,y2);
        this.context.stroke();
     }
 
-    drawBorder() {
+    drawBorder() {              //function for room borders drawing
         this.drawLine(0, 0, this.canvas.width, 0);
         this.drawLine(0, 0, 0, this.canvas.height);
         this.drawLine(this.canvas.width, 0, this.canvas.width, this.canvas.height);
@@ -27,30 +42,33 @@ class Room {
         console.log ('Drawing gamefield');
    }
 
-    draw(){
+    draw(){         //drawinr room itself: borders, door, obstacles
        this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
        this.drawBorder();
 
-       for (let y = 3; y < 64; y = y + 8) {
-           this.drawObstacle(Math.floor(Math.random() * (this.extent - 8)), y);
+       for (let i = 0; i < 8; i++) {
+           this.drawObstacle(this.randCoord[i]);
        }   
            this.door.draw();
        }
 
        
 
-   drawObstacle(x,y) {
+   drawObstacle(Coord) {    // function for obstacles drawing
        this.context.fillStyle = 'black';
        this.context.fillRect(
-           x*this.cellSize, 
-           y*this.cellSize, 
-           this.cellSize*4, 
-           this.cellSize*4);
+           Coord.x*this.cellSize, 
+           Coord.y*this.cellSize, 
+           this.cellSize*this.extent/16, 
+           this.cellSize*this.extent/16);
        }
+
+    update() {          // seems like we don't need this function right now. Room updates in Room.draw().
+    }
 
 }
 
-class Door {
+export class Door {
    constructor (colour, state, position, extent, cellSize, context) {
        this.colour = colour;
        this.state = state;
@@ -60,17 +78,17 @@ class Door {
        this.context = context;
    }
 
-   draw() {
+   draw() {         //function for drawing a door
        this.context.fillStyle = this.colour;
        this.context.fillRect(
            this.position.x*this.cellSize, 
            this.position.y*this.cellSize, 
-           100, 
-           30);
+           this.cellSize*this.extent/8, 
+           this.cellSize*this.extent/32);
            }
 
 }
 
-module.exports = Room;
-module.exports = Door;
+// module.exports = Room;
+// module.exports = Door;
 //new Room(document.getElementById('myCanvas'), 64, 0);
