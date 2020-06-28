04f329364d651b5f0c10d1387c479606dd1e14b7
ru64xat <andrei.zubarev@campus.lmu.de>
2020-06-28 17:28:38 +0200

    rendering.js

--- client/Rendering.js
+++ client/Rendering.js
@@ -1,1 +1,11 @@
 // Rendering Room *** Andrei *** 23.6.2020 
+import { Game } from "/Game.js"
+import { Room } from "/Room.js" 
+
+export class Rendering{
+    constructor(room) {
+    this.room = Game.room;
+    setInterval(this.room.draw(), 33) //soll es genug sein einfach room.draw zu aufrufen, weil alle nötige Daten sind schon in Room.js ?
+    }
+    
+}
